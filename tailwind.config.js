db2170bbb0b6687b710839005e1ade09b3df56c0
Cyril-vtn <votion.cyril@gmail.com>
2024-01-29 18:44:01 +0100

    setup colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      white: "#fff",
+      black: "#1B1B1B",
+      grey: "#7A7A7A",
+      lightGrey: "#C6C6C6",
+      yellow: "#FFD15B",
+    },
   },
   plugins: [],
 };
