454f8d80b7840183213ef4c4074ded510f44ef01
Cyril-vtn <votion.cyril@gmail.com>
2024-02-20 09:08:15 +0100

    Add type module to package.json, update content paths in tailwind.config.js, and extend theme with background and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{html,js}",
     "./pages/**/*.{html,js}",
+    "./js/**/*.js",
     "./index.html",
+    "./src/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        header: "url('/assets/images/banner.png')",
+      },
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
     colors: {
       white: "#fff",
       black: "#1B1B1B",
       grey: "#7A7A7A",
       lightGrey: "#C6C6C6",
       yellow: "#FFD15B",
+      background: "#EDEDED",
     },
   },
   plugins: [],
