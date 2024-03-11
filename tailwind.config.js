36c3e52c8747743fec8ce41d01ea39dca4185af9
Cyril-vtn <votion.cyril@gmail.com>
2024-03-11 13:16:54 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{html,js}",
-    "./js/**/*.js",
-    "./index.html",
-    "./src/**/*.html",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        header: "url('/assets/images/banner.png')",
-      },
-      fontFamily: {
-        anton: ["Anton", "sans-serif"],
-        manrope: ["Manrope", "sans-serif"],
-      },
+export const content = [
+  "./pages/**/*.{html,js}",
+  "./js/**/*.js",
+  "./index.html",
+  "./src/**/*.html",
+];
+export const theme = {
+  extend: {
+    backgroundImage: {
+      header: "url('/assets/images/banner.png')",
     },
-    colors: {
-      white: "#fff",
-      black: "#1B1B1B",
-      grey: "#7A7A7A",
-      lightGrey: "#C6C6C6",
-      yellow: "#FFD15B",
-      background: "#EDEDED",
+    fontFamily: {
+      anton: ["Anton", "sans-serif"],
+      manrope: ["Manrope", "sans-serif"],
     },
   },
-  plugins: [],
+  colors: {
+    white: "#fff",
+    black: "#1B1B1B",
+    grey: "#7A7A7A",
+    lightGrey: "#C6C6C6",
+    yellow: "#FFD15B",
+    background: "#EDEDED",
+  },
 };
+export const plugins = [];
